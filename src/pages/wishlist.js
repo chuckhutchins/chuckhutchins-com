f1d94f62566afa54dc052bf379b89530e9cf4b5f
Chuck Hutchins <chutchins@wayne.edu>
2020-11-02 18:34:54 -0500

    switching wishlist layout to be a product grid

--- src/pages/wishlist.js
+++ src/pages/wishlist.js
@@ -15,33 +15,141 @@ const IndexPage = () => (
       <div className="hr--mini" />
       <div>
         <h3>Fun Things</h3>
-        <ul className="list">
-          <li><a href="https://www.fjallraven.com/us/en-us/bags-gear/accessories/backpack-bag-accessories/kanken-organizer">Kånken Organizer, Fog</a> - Because they don't have black :(</li>
-          <li><a href="https://openseadesignco.com/collections/art-prints/products/rebirth">Rebirth Print</a></li>
-          <li><a href="https://www.fjallraven.com/us/en-us/bags-gear/accessories/travel-accessories/zip-card-holder">Zip Card Holder, Dark Grey</a> - This is so I can store emergency cash and/or "conditional" cards in my backpack</li>
-          <li><a href="https://www.fjallraven.com/us/en-us/bags-gear/accessories/travel-accessories/leather-luggage-tag">Leather Luggage Tag, Black</a> - If we ever get to travel again</li>
-          <li><a href="https://www.fjallraven.com/us/en-us/kids/accessories/kanken-keyring">Kånken Keyring, Black (x2)</a></li>
-          <li><a href="https://www.vizio.com/tvs/p659g1.html">Vizio P-Series Quantum 65" Class 4K HDR Smart TV, P659-G1</a> - I need to do more TV research because I bookmarked this a long time ago</li>
-          <li><a href="https://www.rei.com/product/128052/nemo-stargaze-recliner-luxury-chair">Nemo Stargaze Recliner Luxury Chair, Graphite/Smoke</a></li>
-          <li><a href="https://www.vanmoof.com/en-US/s3?color=dark">VanMoof S3</a></li>
-          <li><a href="https://shop.thejamesbrand.com/products/the-hardin?variant=30552406851654">The Hardin Carabiner, Black</a></li>
-          <li><a href="https://bellroy.com/products/key-cover/leather_standard/black">Bellroy Key Cover, Black</a></li>
-          <li><a href="https://www.amazon.com/gift-cards/b/">Amazon Gift Card</a> - So I can purchase books on my Kindle account</li>
-          <li><a href="https://www.amazon.com/All-new-Kindle-Oasis-adjustable-auto-renewal/dp/B07VDMJJLG/">Kindle Oasis, Graphite</a></li>
-          <li><a href="https://acaia.co/collections/coffee-scales/products/pearl?variant=3791487303703">Acaia Pearl, Black</a></li>
-          <li><a href="https://www.apple.com/shop/buy-homepod/homepod-mini">HomePod mini, Space Gray</a></li>
+        <ul className="product__container">
+          <li>
+            <a href="https://www.fjallraven.com/us/en-us/bags-gear/accessories/backpack-bag-accessories/kanken-organizer">
+              <img src="https://res.cloudinary.com/innocent/image/upload/f_auto/v1604359037/chuckhutchins/kanken-organizer-fog_ejjusb.png" alt=""/>
+              <h4>Kånken Organizer, Fog</h4>
+              <p>Because they don't have black :(</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://openseadesignco.com/collections/art-prints/products/rebirth">
+              <img src="https://placehold.it/330x330" alt=""/>
+              <h4>Rebirth Print</h4>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.fjallraven.com/us/en-us/bags-gear/accessories/travel-accessories/zip-card-holder">
+              <img src="https://placehold.it/330x330" alt=""/>
+              <h4>Zip Card Holder, Dark Grey</h4>
+              <p>This is so I can store emergency cash and/or "conditional" cards in my backpack</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.fjallraven.com/us/en-us/bags-gear/accessories/travel-accessories/leather-luggage-tag">
+              <img src="https://placehold.it/330x330" alt=""/>
+              <h4>Leather Luggage Tag, Black</h4>
+              <p>If we ever get to travel again</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.fjallraven.com/us/en-us/kids/accessories/kanken-keyring">
+              <img src="https://placehold.it/330x330" alt=""/>
+              <h4>Kånken Keyring, Black (x2)</h4>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.vizio.com/tvs/p659g1.html">
+              <img src="https://placehold.it/330x330" alt=""/>
+              <h4>Vizio P-Series Quantum 65" Class 4K HDR Smart TV, P659-H1</h4>
+              <p>I need to do more TV research because I bookmarked this a long time ago</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.rei.com/product/128052/nemo-stargaze-recliner-luxury-chair">
+              <img src="https://placehold.it/330x330" alt=""/>
+              <h4>Nemo Stargaze Recliner Luxury Chair, Graphite/Smoke</h4>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.vanmoof.com/en-US/s3?color=dark">
+              <img src="https://placehold.it/330x330" alt=""/>
+              <h4>VanMoof S3</h4>
+            </a>
+          </li>
+          <li>
+            <a href="https://shop.thejamesbrand.com/products/the-hardin?variant=30552406851654">
+              <img src="https://placehold.it/330x330" alt=""/>
+              <h4>The Hardin Carabiner, Black</h4>
+            </a>
+          </li>
+          <li>
+            <a href="https://bellroy.com/products/key-cover/leather_standard/black">
+              <img src="https://placehold.it/330x330" alt=""/>
+              <h4>Bellroy Key Cover, Black</h4>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.amazon.com/gift-cards/b/">
+              <img src="https://placehold.it/330x330" alt=""/>
+              <h4>Amazon Gift Card</h4>
+              <p>So I can purchase books on my Kindle account</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.amazon.com/All-new-Kindle-Oasis-adjustable-auto-renewal/dp/B07VDMJJLG/">
+              <img src="https://placehold.it/330x330" alt=""/>
+              <h4>Kindle Oasis, Graphite</h4>
+            </a>
+          </li>
+          <li>
+            <a href="https://acaia.co/collections/coffee-scales/products/pearl?variant=3791487303703">
+              <img src="https://placehold.it/330x330" alt=""/>
+              <h4>Acaia Pearl, Black</h4>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.apple.com/shop/buy-homepod/homepod-mini">
+              <img src="https://placehold.it/330x330" alt=""/>
+              <h4>HomePod mini, Space Gray</h4>
+            </a>
+          </li>
         </ul>
         <h3>Developer Things</h3>
-        <ul className="list">
-          <li><a href="https://mastergatsby.com/">Master Gatsby Course</a> - I will be buying this so it links to my account</li>
-          <li><a href="https://www.macbartender.com/">Bartender 3</a> - I will be buying this so it links to my account</li>
+        <ul className="product__container">
+          <li>
+            <a href="https://mastergatsby.com/">
+              <img src="https://placehold.it/330x330" alt=""/>
+              <h4>Master Gatsby Course</h4>
+              <p>I will be buying this so it links to my account</p>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.macbartender.com/">
+              <img src="https://placehold.it/330x330" alt=""/>
+              <h4>Bartender 3</h4>
+              <p>I will be buying this so it links to my account</p>
+          </a>
+          </li>
         </ul>
         <h3>Office Things</h3>
-        <ul className="list">
-          <li><a href="https://blackwing602.com/collections/pencils/products/blackwing-natural-set-of-12">Blackwing Natural Pencils</a></li>
-          <li><a href="https://www.yubico.com/product/yubikey-5c-nfc/">YubiKey 5C NFC</a></li>
-          <li><a href="https://www.samsung.com/us/computing/memory-storage/portable-solid-state-drives/portable-ssd-t7-usb-3-2-500gb--gray--mu-pc500t-am/">Portable SSD T7, Gray</a> - Need to revisit this because I want to find a USB-C model</li>
-          <li><a href="https://store.hermanmiller.com/office/office-chairs/aeron-chair/2195348.html">Herman Miller Aeron Chair</a></li>
+        <ul className="product__container">
+          <li>
+            <a href="https://blackwing602.com/collections/pencils/products/blackwing-natural-set-of-12">
+              <img src="https://placehold.it/330x330" alt=""/>
+              <h4>Blackwing Natural Pencils</h4>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.yubico.com/product/yubikey-5c-nfc/">
+              <img src="https://placehold.it/330x330" alt=""/>
+              <h4>YubiKey 5C NFC</h4>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.samsung.com/us/computing/memory-storage/portable-solid-state-drives/portable-ssd-t7-usb-3-2-500gb--gray--mu-pc500t-am/">
+              <img src="https://placehold.it/330x330" alt=""/>
+              <h4>Portable SSD T7, Gray</h4>
+              <p>Need to revisit this because I want to find a USB-C model</p>
+          </a>
+          </li>
+          <li>
+            <a href="https://store.hermanmiller.com/office/office-chairs/aeron-chair/2195348.html">
+              <img src="https://placehold.it/330x330" alt=""/>
+              <h4>Herman Miller Aeron Chair</h4>
+            </a>
+          </li>
         </ul>
       </div>
     </main>
