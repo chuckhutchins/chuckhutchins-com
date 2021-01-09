1b9fb3ca1c8cef6ce04343b2b96c318ccd665e0a
Chuck Hutchins <chutchins@wayne.edu>
2021-01-09 14:40:51 -0500

    adding blank page for p5r guide

---
+++ src/pages/p5r/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
+
+const IndexPage = () => (
+    <Layout>
+        <main className="wishlist">
+            <SEO title={"Persona 5 Royal"}/>
+            <div>
+                <h2>Persona 5 Royal Guides</h2>
+                <p>Wow this is way more convenient than what I was doing before.</p>
+            </div>
+            <div className="hr--mini"/>
+            <div>
+
+            </div>
+        </main>
+    </Layout>
+)
+
+export default IndexPage
