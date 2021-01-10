11b372fc56132c9e5286c3febba3980d81c6eee0
Chuck Hutchins <chutchins@wayne.edu>
2021-01-10 17:20:45 -0500

    scaffolding out the rest of the pages for each month

---
+++ src/pages/p5r/09/index.js
@@ -0,0 +1,107 @@
+import React from "react"
+
+import Layout from "../../../components/layout-p5r"
+import SEO from "../../../components/seo"
+
+const IndexPage = () => (
+    <Layout>
+        <main>
+            <SEO title={"September - Persona 5 Royal"}/>
+            <div>
+                <h2>September</h2>
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+            <div className="day">
+
+            </div>
+        </main>
+    </Layout>
+)
+
+export default IndexPage
