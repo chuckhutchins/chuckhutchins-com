2bb2eff0d1d35f246393893b03ba89252e273715
Chuck Hutchins <chuck@chuckhutchins.com>
2021-01-30 11:40:26 -0500

    adding books and video games markdown files as pages, defining type schemas for markdown frontmatter

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,42 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+exports.createPages = async ({ actions, graphql }) => {
+    const { createPage } = actions;
+    const { data } = await graphql(`
+        {
+            files: allFile(filter: {sourceInstanceName: {in: ["books", "video-games"]}}) {
+                edges {
+                    node {
+                        sourceInstanceName
+                        name
+                    }
+                }
+            }
+        }
+    `);
 
-// You can delete this file if you're not using it
+    data.files.edges.forEach(({ node }) => {
+        createPage({
+            path: `media-consumption/${node.sourceInstanceName}/${node.name}`,
+            component: require.resolve(`./src/templates/media-consumption.js`),
+            context: {
+                slug: node.name,
+            },
+        });
+    });
+}
+
+exports.createSchemaCustomization = ({ actions }) => {
+    const { createTypes } = actions;
+    const typeDefs = `
+        type MarkdownRemark implements Node {
+            frontmatter: Frontmatter
+        }
+        type Frontmatter {
+            season: Int
+            finish: Boolean
+            love: Boolean
+            hate: Boolean
+        }
+    `;
+
+    createTypes(typeDefs)
+}
