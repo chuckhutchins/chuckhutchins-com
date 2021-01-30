44c4970e004f9da1ae6eda3c77b94d7c485e3b52
Chuck Hutchins <chuck@chuckhutchins.com>
2021-01-30 11:37:47 -0500

    fixing layout

--- src/pages/p5r/index.js
+++ src/pages/p5r/index.js
@@ -9,35 +9,37 @@ const IndexPage = () => (
         <main>
             <SEO title={"Persona 5 Royal"}/>
             <div>
-                <h2>Persona 5 Royal Guides</h2>
-                <p>Wow this is way more convenient than what I was doing before.</p>
-            </div>
-            <div className="hr--mini"/>
-            <div>
-                <h3>Months</h3>
-                <ul className="list">
-                    <li><Link to="/p5r/04">April</Link></li>
-                    <li><Link to="/p5r/05">May</Link></li>
-                    <li><Link to="/p5r/06">June</Link></li>
-                    <li><Link to="/p5r/07">July</Link></li>
-                    <li><Link to="/p5r/08">August</Link></li>
-                    <li><Link to="/p5r/09">September</Link></li>
-                    <li><Link to="/p5r/10">October</Link></li>
-                    <li><Link to="/p5r/11">November</Link></li>
-                    <li>December</li>
-                    <li>January</li>
-                    <li>February</li>
-                </ul>
-            </div>
-            <div className="hr--mini"/>
-            <div>
-                <h3>Resources</h3>
-                <ul className="list">
-                    <li><Link to="/p5r/changelog">Changelog</Link></li>
-                    <li><a href="https://joyceychen.com/persona5-negotiation/royal">Negotiation Guide</a></li>
-                    <li><a href="https://aqiu384.github.io/p5r-walkthrough/walkthrough">Data Source</a></li>
-                    <li><a href="https://aqiu384.github.io/megaten-fusion-tool/p5r/personas">Fusion Guide</a></li>
-                </ul>
+                <div>
+                    <h2>Persona 5 Royal Guides</h2>
+                    <p>Wow this is way more convenient than what I was doing before.</p>
+                </div>
+                <div className="hr--mini"/>
+                <div>
+                    <h3>Months</h3>
+                    <ul className="list">
+                        <li><Link to="/p5r/04">April</Link></li>
+                        <li><Link to="/p5r/05">May</Link></li>
+                        <li><Link to="/p5r/06">June</Link></li>
+                        <li><Link to="/p5r/07">July</Link></li>
+                        <li><Link to="/p5r/08">August</Link></li>
+                        <li><Link to="/p5r/09">September</Link></li>
+                        <li><Link to="/p5r/10">October</Link></li>
+                        <li><Link to="/p5r/11">November</Link></li>
+                        <li>December</li>
+                        <li>January</li>
+                        <li>February</li>
+                    </ul>
+                </div>
+                <div className="hr--mini"/>
+                <div>
+                    <h3>Resources</h3>
+                    <ul className="list">
+                        <li><Link to="/p5r/changelog">Changelog</Link></li>
+                        <li><a href="https://joyceychen.com/persona5-negotiation/royal">Negotiation Guide</a></li>
+                        <li><a href="https://aqiu384.github.io/p5r-walkthrough/walkthrough">Data Source</a></li>
+                        <li><a href="https://aqiu384.github.io/megaten-fusion-tool/p5r/personas">Fusion Guide</a></li>
+                    </ul>
+                </div>
             </div>
         </main>
     </Layout>
