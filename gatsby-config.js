a969ef9646c0f0b8b7263f94bb54773ba0af2d90
Chuck Hutchins <chuck@chuckhutchins.com>
2021-01-30 11:39:22 -0500

    adding filesystem plugin options, adding markdown support, updating site metadata, fixing indentation

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,49 @@
 module.exports = {
-  siteMetadata: {
-    title: `Chuck Hutchins - Front-End Developer`,
-    description: `Chuck Hutchins is a design-oriented front-end developer from the Metro Detroit area.`,
-    author: `Chuck Hutchins`,
-  },
-  plugins: [
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        title: `Chuck Hutchins`,
+        description: `Chuck Hutchins is a design-oriented front-end developer from the Metro Detroit area.`,
+        author: `Chuck Hutchins`,
+        siteUrl: `https://chuckhutchins.com`
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#111111`,
-        theme_color: `#111111`,
-        display: `minimal-ui`,
-        icon: `src/images/icon-checkers.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+    plugins: [
+        `gatsby-plugin-sass`,
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `books`,
+                path: `${__dirname}/src/pages/media-consumption/books`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `video-games`,
+                path: `${__dirname}/src/pages/media-consumption/video-games`,
+            },
+        },
+        `gatsby-transformer-remark`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#111111`,
+                theme_color: `#111111`,
+                display: `minimal-ui`,
+                icon: `src/images/icon-checkers.png`,
+            },
+        },
+        `gatsby-plugin-mdx`,
+    ],
 }
