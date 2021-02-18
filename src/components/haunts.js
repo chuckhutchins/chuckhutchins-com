a3fcef57560584db794864ed52170a06dd8d4266
Chuck Hutchins <chuck@chuckhutchins.com>
2021-02-17 20:06:41 -0500

    extracting haunts styles into partial from styled component

--- src/components/haunts.js
+++ src/components/haunts.js
@@ -1,23 +1,11 @@
 import React from "react"
-import styled from "styled-components"
-
-const HauntsStyles = styled.ul`
-    display: flex;
-
-    li {
-        line-height: 1;
-
-        + li {
-            margin-left: 1rem;
-        }
-    }
-`;
 
 export default function Haunts() {
     return (
-        <HauntsStyles className="haunts">
+        <ul className="haunts">
             <li className="email"><a href="mailto:chuck@chuckhutchins.com" aria-label="Email Me">chuck@chuckhutchins.com</a></li>
-            <li className="linkedin"><a href="https://www.linkedin.com/in/chuckhutchinsdesign/" aria-label="View Me on LinkedIn">LinkedIn</a></li>
-        </HauntsStyles>
+            <li className="github"><a href="https://github.com/InnocentSmasher" aria-label="View My Profile on GitHub">GitHub</a></li>
+            <li className="linkedin"><a href="https://www.linkedin.com/in/chuckhutchinsdesign/" aria-label="View My Profile on LinkedIn">LinkedIn</a></li>
+        </ul>
     )
 }
