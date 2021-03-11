a120ddbff786bac0f35b86ddfbb48fa3ee0908a8
Chuck Hutchins <chuck@chuckhutchins.com>
2021-03-11 18:10:33 -0500

    adding return to links to individual media list pages

--- src/pages/media-consumption/books/index.js
+++ src/pages/media-consumption/books/index.js
@@ -3,6 +3,7 @@ import MediaLayout from "/src/components/media-consumption/MediaLayout";
 import SEO from "/src/components/seo";
 import MediaList from "/src/components/media-consumption/MediaList";
 import JSONData from "../../../../content/book-list.json"
+import ReturnToLink from "../../../components/media-consumption/ReturnToLink"
 
 export default function IndexPage() {
     return (
@@ -13,9 +14,16 @@ export default function IndexPage() {
                     <h2>Books</h2>
                     <p>Hey, this is all the stuff I've read since 2020.</p>
                 </div>
+                <div>
+                    <ReturnToLink page="media consumption" />
+                </div>
                 <div className="hr--mini"/>
                 <div className="media-consumption__media">
                     <MediaList mediaList={JSONData} />
+                </div>
+                <div className="hr--mini"/>
+                <div>
+                    <ReturnToLink page="media consumption" />
                 </div>
             </main>
         </MediaLayout>
--- src/pages/media-consumption/video-games/index.js
+++ src/pages/media-consumption/video-games/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import MediaLayout from "/src/components/media-consumption/MediaLayout";
 import SEO from "/src/components/seo";
+import ReturnToLink from "/src/components/media-consumption/ReturnToLink";
 import MediaList from "../../../components/media-consumption/MediaList"
 import JSONData from "../../../../content/video-game-list.json";
 
@@ -13,9 +14,16 @@ export default function IndexPage() {
                     <h2>Video Games</h2>
                     <p>Hey, this is all the stuff I've played since 2020.</p>
                 </div>
+                <div>
+                    <ReturnToLink page="media consumption" />
+                </div>
                 <div className="hr--mini"/>
                 <div className="media-consumption__media">
                     <MediaList mediaList={JSONData} />
+                </div>
+                <div className="hr--mini"/>
+                <div>
+                    <ReturnToLink page="media consumption" />
                 </div>
             </main>
         </MediaLayout>
