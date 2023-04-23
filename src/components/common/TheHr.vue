b546c3807ef8dc2819d2e5d00efd53f900c0f300
Chuck Hutchins <46427222+InnocentSmasher@users.noreply.github.com>
2023-04-23 11:15:41 -0400

    adding custom hr component

---
+++ src/components/common/TheHr.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hr" />
+</template>
+
+<script>
+export default {
+  name: 'TheHr',
+};
+</script>
+
+<style scoped lang="scss">
+.hr {
+  width: 2rem;
+  border-bottom: 4px solid;
+}
+</style>
