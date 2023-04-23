50fd4b53afc80516b050d7f5ea8ad741ac1c3830
Chuck Hutchins <46427222+InnocentSmasher@users.noreply.github.com>
2023-04-23 11:15:01 -0400

    adding coffee item component

---
+++ src/components/coffee/CoffeeItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <li>
+    <h2>{{ coffee.name }}</h2>
+    <p>{{ coffee.roaster }}</p>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'CoffeeItem',
+  props: {
+    coffee: {
+      required: true,
+      type: Object,
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+h2 {
+  font-size: 1rem;
+}
+
+p {
+  font-size: 1rem;
+  margin: 0;
+}
+</style>
