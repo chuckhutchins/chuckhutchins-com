a300fd2d525ea2ddf310a0614eda3750ad9a4dbd
Chuck Hutchins <46427222+InnocentSmasher@users.noreply.github.com>
2023-04-23 11:16:34 -0400

    setting up base app

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,174 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+  <div class="container">
+    <header class="header">
+      <nav class="nav">
+        <RouterLink :to="{name: 'ResumePage'}" class="main">Chuck Hutchins</RouterLink>
+        <ul class="haunts">
+          <li>
+            <a href="mailto:chuck@chuckhutchins.com" aria-label="Email Me">chuck@chuckhutchins.com</a>
+          </li>
+          <li>
+            <a href="https://github.com/InnocentSmasher" aria-label="View My Profile on GitHub">GitHub</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/chuckhutchinsdesign/" aria-label="View My Profile on LinkedIn">LinkedIn</a>
+          </li>
+        </ul>
       </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    </header>
+    <RouterView />
+    <footer class="footer">
+      <nav>
+        <ul class="haunts">
+          <li><a href="mailto:chuck@chuckhutchins.com" aria-label="Email Me">chuck@chuckhutchins.com</a></li>
+          <li><a href="https://github.com/InnocentSmasher" aria-label="View My Profile on GitHub">GitHub</a></li>
+          <li><a href="https://www.linkedin.com/in/chuckhutchinsdesign/" aria-label="View My Profile on LinkedIn">LinkedIn</a></li>
+        </ul>
+      </nav>
+      <div>
+        &copy; {{ currentYear }} Chuck Hutchins
+      </div>
+    </footer>
+  </div>
 </template>
 
-<style scoped>
+<script>
+export default {
+  name: 'App',
+  computed: {
+    currentYear() {
+      return new Date().getFullYear();
+    }
+  },
+};
+</script>
+
+<style scoped lang="scss">
+// TODO: fix header/footer styles for mobile
+// TODO: extract header/footer into components
+// TODO: experiment with heavier weights
+//nav {
+//  width: 100%;
+//  font-size: 12px;
+//  text-align: center;
+//  margin-top: 2rem;
+//
+//  a.router-link-exact-active {
+//    color: var(--color-text);
+//  }
+//
+//  a.router-link-exact-active:hover {
+//    background-color: transparent;
+//  }
+//
+//  a {
+//    display: inline-block;
+//    padding: 0 1rem;
+//    border-left: 1px solid var(--color-border);
+//  }
+//
+//  a:first-of-type {
+//    border: 0;
+//  }
+//}
+.container {
+  width: calc(100% - 2rem);
+  max-width: 1024px;
+  margin: 0 auto;
+  box-sizing: border-box;
+  padding: 1rem 0;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  align-items: start;
+  grid-gap: 2rem;
+  min-height: 100dvh;
+}
+
+.not-found {
+  h1 {
+    margin-bottom: .5rem;
+  }
+}
+
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+  border-bottom: 2px solid;
+  padding-bottom: 1rem;
+
+  .nav {
+    display: flex;
+    gap: 2rem;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+
+    @media (min-width: 768px) {
+      flex-direction: row;
+    }
+
+    .main {
+      font-size: 1.5rem;
+      font-weight: 700;
+      line-height: 1;
+      letter-spacing: -.0125em;
+      text-decoration: none;
+      border-bottom: transparent;
+    }
+  }
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+.haunts {
+  $haunts-breakpoint: 424px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 1rem;
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  @media (min-width: $haunts-breakpoint) {
+    width: unset;
+    grid-template-columns: repeat(3, min-content);
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  li {
+    line-height: 1;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .email {
+    grid-column: span 2;
+    justify-self: center;
+
+    @media (min-width: $haunts-breakpoint) {
+      grid-column: span 1;
+    }
   }
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
+  .github {
+    justify-self: self-end;
+  }
 
-    padding: 1rem 0;
-    margin-top: 1rem;
+  .linkedin {
+    justify-self: self-start;
+  }
+}
+
+footer {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  border-top: 2px solid;
+  padding-top: 1rem;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+
+  .copyright {
+    line-height: 1;
+    margin-top: 1.5rem;
+
+    @media (min-width: 768px) {
+      margin-top: 0;
+    }
   }
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import ResumeView from '@/views/ResumeView.vue';
+import {useCoffeeStore} from '@/stores/CoffeeStore.js';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'ResumePage',
+      component: ResumeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/coffee-consumption',
+      name: 'CoffeeConsumptionPage',
+      component: () => import('@/views/CoffeeConsumptionView.vue'),
+      beforeEnter(to, from, next) {
+        const coffeeStore = useCoffeeStore();
+        coffeeStore.getCoffeeList();
+        next();
+      },
+    },
+    {
+      path: '/media-consumption',
+      name: 'MediaConsumptionPage',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      component: () => import('@/views/MediaConsumptionView.vue'),
+    },
+  ],
+});
 
-export default router
+export default router;
