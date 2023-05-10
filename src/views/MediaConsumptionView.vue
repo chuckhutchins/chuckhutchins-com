a126f5bbb65bb43eb3ca158e703ec7f1097eaa87
Chuck Hutchins <46427222+InnocentSmasher@users.noreply.github.com>
2023-05-10 19:54:48 -0400

    adding media consumption list components

--- src/views/MediaConsumptionView.vue
+++ src/views/MediaConsumptionView.vue
@@ -1,11 +1,52 @@
 <template>
-  <main>
-    media
+  <main class="consumption">
+    <div class="opener">
+      <h1>Media Consumption</h1>
+      <p>Hey, this is all the stuff I've read/watched/played since 2020.</p>
+    </div>
+    <TheHr />
+    <div class="type-wrapper">
+      <ul class="type-list">
+        <MediaTypeItem type="Books" />
+        <MediaTypeItem type="Movies" />
+        <MediaTypeItem type="TV Shows" />
+        <MediaTypeItem type="Video Games" />
+      </ul>
+    </div>
   </main>
 </template>
 
 <script>
+import MediaTypeItem from '@/components/media/MediaTypeItem.vue';
+import TheHr from '@/components/common/TheHr.vue';
+
 export default {
   name: 'MediaConsumptionView',
+  components: {
+    MediaTypeItem,
+    TheHr,
+  },
 };
 </script>
+
+<style scoped lang="scss">
+.consumption {
+  display: grid;
+  gap: 2rem;
+
+  .opener {
+    h1 {
+      margin-block-end: 1rem;
+    }
+
+    p:not(:last-child) {
+      margin-block-end: 0.5rem;
+    }
+  }
+
+  .type-list {
+    display: grid;
+    gap: 1rem;
+  }
+}
+</style>
