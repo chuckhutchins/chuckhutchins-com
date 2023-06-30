4c2e9f3116c218f4a23a73b045a22873179fe7cd
Chuck Hutchins <46427222+chuckhutchins@users.noreply.github.com>
2023-06-29 20:14:24 -0400

    removing old gatsby files

--- _src/components/media-consumption/ReturnToLink.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Link } from "gatsby";
-
-export default function Media({ page }) {
-
-    return (
-        <Link to=".." className="return-to">Return to {page}</Link>
-    );
-}
