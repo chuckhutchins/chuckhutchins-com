ae286dc189e64f9f8aac3bc3d017b0b427c57184
Chuck Hutchins <46427222+chuckhutchins@users.noreply.github.com>
2025-01-28 14:55:56 -0500

    updating media lists

--- src/assets/data/video-game-list.ts
+++ src/assets/data/video-game-list.ts
@@ -1069,6 +1069,16 @@ export const videoGameList = [
     rating: '',
     finish: false,
   },
+  {
+    index: 107,
+    title: 'Pokémon Yellow Legacy',
+    platform: 'Emulator',
+    image: 'https://res.cloudinary.com/innocent/image/upload/f_auto/v1688421140/chuckhutchins/video-games/pokemon-yellow-legacy_ypvczd.png',
+    start: '2025-01-17',
+    end: '',
+    rating: '',
+    finish: false,
+  },
   // {
   //   index: 107,
   //   title: 'Persona 3 Reload: Episode Aigis',
