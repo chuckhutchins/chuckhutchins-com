5f4310b7a3144837121a7f836162d7a72d774423
Chuck Hutchins <46427222+chuckhutchins@users.noreply.github.com>
2025-02-17 12:31:14 -0500

    updating content

---
+++ src/assets/data/black-coffee-list.ts
@@ -0,0 +1,16 @@
+export const coffeeList = [
+  {
+    index: 1,
+    name: 'French Roast',
+    roaster: 'Orinoco Coffee & Tea',
+    rating: 4,
+  },
+  {
+    index: 2,
+    name: 'Red Honey',
+    roaster: 'Kohi Coffee',
+    rating: 3,
+    region: 'Carrazu',
+    country: 'Costa Rica',
+  },
+];
